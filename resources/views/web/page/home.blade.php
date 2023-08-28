5b75d2aae322184390aa2ffc3244bab8cf37469f
fatlc24 <nghiemquanganh2014@gmail.com>
2023-08-28 15:25:38 +0700

    sua trang gioi thieu

--- resources/views/web/page/home.blade.php
+++ resources/views/web/page/home.blade.php
@@ -1,32 +1,32 @@
 @extends('web.layouts.web')
 
 @section('content')
-    <div class="content-detail">
-        <div class="image-banner">
-            @include('web.components.image', ['src' => rawurldecode($page->image_title), 'title' => $page->title])
-        </div>
-        <div class="container">
-            <div class="box-content-detail">
-                <div class="show-content ck-content">
-                    {!! $page->content !!}
-                </div>
-            </div>
-        </div>
+<div class="content-detail">
+    <div class="image-banner">
+        @include('web.components.image', ['src' => rawurldecode($page->image_title), 'title' => $page->title])
     </div>
+    <div class="container">
+        <div class="box-content-detail">
+            <div class="show-content ck-content">
+                {!! $page->content !!}
+            </div>
+        </div>`
+    </div>
+</div>
 @endsection
 
 @section('link')
-    @parent
-    <link rel="stylesheet" href="{{ asset('/css/web/page-home.css') }}">
-    <link rel="stylesheet" href="{{ asset('/css/content-ckeditor.css') }}">
+@parent
+<link rel="stylesheet" href="{{ asset('/css/web/page-home.css') }}">
+<link rel="stylesheet" href="{{ asset('/css/content-ckeditor.css') }}">
 @endsection
 
 @section('script')
-    @parent
-    <script async src="//cdn.iframe.ly/embed.js"></script>
-    <script>
-        document.querySelectorAll( 'oembed[url]' ).forEach( element => {
-            iframely.load( element, element.attributes.url.value );
-        } );
-    </script>
+@parent
+<script async src="//cdn.iframe.ly/embed.js"></script>
+<script>
+document.querySelectorAll('oembed[url]').forEach(element => {
+    iframely.load(element, element.attributes.url.value);
+});
+</script>
 @endsection
